19a3f10adc2fcf0fe0f7104ed269254a5c8c6d17
abelhasratyan <abelhasratyan@gmail.com>
2019-11-11 18:44:37 +0400

    fixed issue with connection uploading photo

--- src/store/modules/posts/actions.js
+++ src/store/modules/posts/actions.js
@@ -7,7 +7,7 @@ export const actions = {
   //  const userId = data.userId
     const formData = new FormData()
     if (file) {
-        formData.append('file', file)
+        formData.append('file', file[0])
        // formData.append('text', text)
        //formData.append('userId',userId)
         
@@ -16,13 +16,13 @@ export const actions = {
             message: 'ERROR',
             success: false
         }
-    }
-    const response = await axios.post('/user/newpost', formData, {
+    } 
+    const response = await axios.post('/uploadfile', formData, {
       headers: {
         //'Content-Type': 'multipart/form-data',
         "Authorization": data.token
       }
-    })
+    }) ; console.log(formData,'HEAD')
 
     if (response.data.success) {
       commit('ADDPOST', response.data)
