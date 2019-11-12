7a92d8462594d100bdc6113a27ed8b2aad91610d
abelhasratyan <abelhasratyan@gmail.com>
2019-11-12 10:25:52 +0400

    fix error

--- src/store/modules/posts/actions.js
+++ src/store/modules/posts/actions.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 export const actions = {
   async AddNewPost({ commit }, data) {
     const file = data.postImg
-   // const text = data.postValue
-  //  const userId = data.userId
+    const text = data.postValue
+    const userId = data.userId
     const formData = new FormData()
     if (file) {
         formData.append('file', file[0])
-       // formData.append('text', text)
-       //formData.append('userId',userId)
+        formData.append('text', text)
+        formData.append('userId',userId)
         
     } else {
         return {
@@ -17,7 +17,7 @@ export const actions = {
             success: false
         }
     } 
-    const response = await axios.post('/uploadfile', formData, {
+    const response = await axios.post('/user/newpost', formData, {
       headers: {
         //'Content-Type': 'multipart/form-data',
         "Authorization": data.token
