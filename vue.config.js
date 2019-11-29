7811a22a9eaf3d7fdd6eac1ad21ae5ce784296a8
abelhasratyan <abelhasratyan@gmail.com>
2019-11-29 15:39:54 +0400

    test

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,10 @@ module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
   devServer: {
-    proxy: 'http://18.185.170.105:7071'
+    proxy: 'http://18.185.170.105:7071',
     // proxy: process.env.SERVER_URL
+  },
+  prodServer:{
+    proxy: 'http://18.185.170.105:7071',
   }
-}
+};
